7f92c8f40169af92155d8b384005e55fc3bf2cf2
DoDangKh <107110364+DoDangKh@users.noreply.github.com>
2024-04-26 17:12:45 +0700

    Add Carousel
    Using Ant Design to create a carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Carousel } from 'antd'
 
 export default function Home() {
+  const contentStyle: React.CSSProperties = {
+    margin: 0,
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <div>
         {/* <navbar/> */}
 
+        <Carousel autoplay>
+          <div>
+            <img src='https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/latest1.jpg'></img>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
         {/* <h1>
           Hello
         </h1>
